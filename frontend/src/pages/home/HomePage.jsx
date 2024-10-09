a314dbe99abeeca5a8a7cf8de9d03ba43b565139
JyothsnaJanjanam <janjanamjyothsna@gmail.com>
2024-10-09 16:16:18 +0530

    initial commit

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,11 +1,10 @@
 import { useAuthStore } from "../../store/authUser";
 import AuthScreen from "./AuthScreen";
-import HomeScreen from "./Homescreen";
+import HomeScreen from "./HomeScreen";
 
 const HomePage = () => {
-  const {user} = useAuthStore();
+	const { user } = useAuthStore();
 
-  return <>{user ? <HomeScreen /> : <AuthScreen/>}</>;
-}
-
-export default HomePage
+	return <>{user ? <HomeScreen /> : <AuthScreen />}</>;
+};
+export default HomePage;
